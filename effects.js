b5d1ba3f31644fdf743a359c3941a4f3e651a463
Revy <157729677+revyian@users.noreply.github.com>
2025-03-02 22:29:39 +0100

    Update effects.js

--- effects.js
+++ effects.js
@@ -61,7 +61,7 @@ typeWriter();
 
 document.addEventListener("DOMContentLoaded", function () {
 const elements = document.querySelectorAll('.typewriter');
-const texts = ["email me: i@clu.ng", "i love kira", "#999"];
+const texts = ["haram income", "high and rich", ":3"];
 const typingSpeed = 100;
 const pauseDuration = 1000;
 let currentIndex = 0;
