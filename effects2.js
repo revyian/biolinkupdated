f71fb4ff55032d19eb38d1b79793d4b4ee41e513
Revy <157729677+revyian@users.noreply.github.com>
2025-05-14 02:15:08 +0200

    Update effects2.js

--- effects2.js
+++ effects2.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const imgSrc = "https://lanyard.cnrad.dev/api/335524602220380170?bg=&borderRadius=&idleMessage=making%20money&theme=yes";
+    const imgSrc = "https://lanyard.cnrad.dev/api/335524602220380170?bg=&theme=dark&borderRadius=&idleMessage=";
     const imgElement = document.getElementById('rpc');
     imgElement.src = imgSrc;
 });
